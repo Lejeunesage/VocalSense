98e51157a01303a59b41a7a6f27e990569286020
Lejeunesage <geoffroyotegbeye@gmail.com>
2024-01-06 14:54:24 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # VocalSense
 VocalSense retranscrit et analyse automatiquement 100% de vos conversations - appels, sms, emails, chats - pour vous permettre d’améliorer en continu la conformité du discours, les taux de conversion et la satisfaction client
+
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+  <script>
+  
+  
+  
+  </script>
+<template>
+  <img alt="Vue logo" src="./assets/vocalsense.png" class="w-40">
+ <div class="text-slate-400 text-2xl font-semibold">
+  Bienvenue sur <span class="text-slate-600 font-extrabold ">VocalSense Analytics</span>, l'assistant conversationnel intelligent qui donne un sens à vos échanges vocaux. 
+ </div>
+</template>
+
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
+#app {
+  font-family: 'Nunito', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+ 
+  color: #2c3e50;
+  
+}
+</style>
